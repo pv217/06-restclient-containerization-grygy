902830dfeccfa53078abb5fe639f016bc6730feb
Adam Grygar <grygarad@gmail.com>
2024-04-24 22:41:41 +0200

    init assignment

---
+++ passenger-service/src/main/java/cz/muni/fi/airportmanager/passengerservice/client/BaggageClientResource.java
@@ -0,0 +1,24 @@
+package cz.muni.fi.airportmanager.passengerservice.client;
+
+import cz.muni.fi.airportmanager.passengerservice.model.Baggage;
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.util.List;
+
+/**
+ * Client for baggage service
+ */
+// TODO add @Path annotation that will specify the path to the baggage resource
+// TODO add @RegisterRestClient annotation. Don't forget to specify the configKey
+public interface BaggageClientResource {
+
+    // TODO add getBaggageForPassengerId method that will get baggage for passenger with given id
+    // It should take one @PathParam argument passengerId
+    // The method should be asynchronous and return Uni
+    // TODO add REST method annotation that will correspond to HTTP method of the baggage resource
+    // TODO add @Path annotation that will specify the path for getting baggage for passenger
+}
---
+++ passenger-service/src/main/java/cz/muni/fi/airportmanager/passengerservice/service/PassengerService.java
@@ -0,0 +1,122 @@
+package cz.muni.fi.airportmanager.passengerservice.service;
+
+import cz.muni.fi.airportmanager.passengerservice.client.BaggageClientResource;
+import cz.muni.fi.airportmanager.passengerservice.entity.Notification;
+import cz.muni.fi.airportmanager.passengerservice.entity.Passenger;
+import cz.muni.fi.airportmanager.passengerservice.model.CreatePassengerDto;
+import cz.muni.fi.airportmanager.passengerservice.model.PassengerWithBaggageDto;
+import cz.muni.fi.airportmanager.passengerservice.repository.PassengerRepository;
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.util.List;
+
+@ApplicationScoped // This bean will be created once per application and live as long as the application lives
+public class PassengerService {
+
+    @Inject
+    PassengerRepository passengerRepository;
+
+    @RestClient
+    BaggageClientResource baggageClientResource;
+
+
+    /**
+     * Get list of all passengers
+     *
+     * @return list of all passengers
+     */
+    @WithTransaction
+    public Uni<List<Passenger>> listAll() {
+        return passengerRepository.listAll();
+    }
+
+    /**
+     * Get passenger by id
+     *
+     * @param id passenger id
+     * @return passenger with given id
+     */
+    @WithTransaction
+    public Uni<Passenger> getPassenger(Long id) {
+        return passengerRepository.findById(id);
+    }
+
+    /**
+     * Get passengers for given flight id
+     *
+     * @param flightId flight id
+     * @return list of passengers for given flight id
+     */
+    @WithTransaction
+    public Uni<List<Passenger>> getPassengersForFlight(Long flightId) {
+        return passengerRepository.findPassengersForFlight(flightId);
+    }
+
+    /**
+     * Create a new passenger
+     *
+     * @param passenger passenger to create.
+     * @return created passenger
+     */
+    @WithTransaction
+    public Uni<Passenger> createPassenger(CreatePassengerDto passenger) {
+        return passengerRepository.persist(Passenger.fromDto(passenger));
+    }
+
+    /**
+     * Delete passenger
+     *
+     * @param id passenger id
+     */
+    @WithTransaction
+    public Uni<Boolean> deletePassenger(Long id) {
+        return passengerRepository.deleteById(id);
+    }
+
+    /**
+     * Delete all passengers
+     */
+    @WithTransaction
+    public Uni<Long> deleteAllPassengers() {
+        return passengerRepository.deleteAll();
+    }
+
+    /**
+     * Add notification to a passengers with given flight id
+     *
+     * @param flightId     flight id
+     * @param notification notification to add
+     */
+    @WithTransaction
+    public Uni<Void> addNotificationByFlightId(Long flightId, Notification notification) {
+        return passengerRepository.addNotificationByFlightId(flightId, notification);
+    }
+
+
+    /**
+     * Get all notifications for passenger
+     *
+     * @param passengerId passenger id
+     * @return list of notifications for passenger
+     */
+    @WithTransaction
+    public Uni<List<Notification>> findNotificationsForPassenger(Long passengerId) {
+        return passengerRepository.findNotificationsForPassenger(passengerId);
+    }
+
+    /**
+     * Get passenger passenger with baggage
+     *
+     * @param passengerId passenger id
+     * @return passenger with baggage
+     * @throws RuntimeException if baggage service fails
+     */
+    @WithTransaction
+    public Uni<PassengerWithBaggageDto> getPassengerWithBaggage(Long passengerId) {
+        // TODO implement this method. It should get passenger and call rest client to get baggage
+    }
+}
